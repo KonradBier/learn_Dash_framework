df7111845e87ffd5bf04bb884885d04d34804dce
KonradBier <jmmpad@gmail.com>
2020-09-11 19:40:30 +0200

    made HTML

--- 02_dash_intro/02_first_app_style.py
+++ 02_dash_intro/02_first_app_style.py
@@ -17,10 +17,40 @@ app.layout = html.Div([
     html.H2('Hello dash',
             style={'color': colors['text'],
                    'textAlign': 'center'}
+            ),
+
+    html.Div('Dash: A web application framework for Python',
+             style={
+                 'color': colors['text'],
+                 'textAlign': 'center'
+             }),
+
+    dcc.Graph(
+        figure=go.Figure([
+            go.Bar(
+                x=[1,2,3],
+                y=[150,180,220],
+                marker_color= '#bf86bf',
+                marker_line_color='#4f544e',
+                marker_line_width= 5,
+                name='lokalna'
+            ),
+            go.Bar(
+                x=[1,2,3],
+                y=[140,190,250],
+                marker_color= '#30d992',
+                marker_line_color='#4f544e',
+                marker_line_width= 5,
+                name='online'
+            )],
+            layout= go.Layout(
+                title='Wizualizacja danych',
+                plot_bgcolor=colors['background']
             )
 
-    html.Div('Dash: A web application framework for Python')
-])
+        )
+    )
+], style={'backgroundColor': colors['background']})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
